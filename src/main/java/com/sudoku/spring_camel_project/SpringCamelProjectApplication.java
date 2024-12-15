c6a38640648e24e66edfae957193870e2e071e20
Dereck Javier Curtis Albizú <drkjavier@gmail.com>
2024-12-14 22:14:38 -0600

    Initial Commit

---
+++ src/main/java/com/sudoku/spring_camel_project/SpringCamelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sudoku.spring_camel_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamelProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudoku/spring_camel_project/SpringCamelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoku.spring_camel_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamelProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
